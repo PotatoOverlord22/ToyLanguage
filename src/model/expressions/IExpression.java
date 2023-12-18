3fc4f628d75f4db7a618cb5693f4cc85eaa6d550
PotatoOverlord22 <raul.tiron@stud.ubbcluj.ro>
2023-12-18 15:28:24 +0200

    modified evaluate signature

--- src/model/expressions/IExpression.java
+++ src/model/expressions/IExpression.java
@@ -2,11 +2,12 @@ package model.expressions;
 
 import model.adts.IMyDictionary;
 import model.adts.IMyHeap;
+import model.adts.SymbolTable;
 import model.exceptions.EvaluationException;
 import model.values.IValue;
 
 public interface IExpression {
-    IValue evaluate(IMyDictionary<String, IValue> table, IMyHeap heap) throws EvaluationException;
+    IValue evaluate(SymbolTable table, IMyHeap heap) throws EvaluationException;
 
     IExpression deepCopy();
 }
