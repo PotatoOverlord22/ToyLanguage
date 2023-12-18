f3c0768633ad8499f563045eb30a5ded0917866e
PotatoOverlord22 <raul.tiron@stud.ubbcluj.ro>
2023-12-18 15:38:40 +0200

    moved one step here

--- src/model/ProgramState.java
+++ src/model/ProgramState.java
@@ -1,6 +1,9 @@
 package model;
 
 import model.adts.*;
+import model.exceptions.EvaluationException;
+import model.exceptions.ExecutionException;
+import model.exceptions.ReadWriteException;
 import model.statements.IStatement;
 import model.values.IValue;
 
@@ -8,8 +11,11 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
 public class ProgramState {
+    private static int idCounter = 0; // all PrgStates will see this
+    private final int id; // Personal id for each ProgramState
+    private static final Object idLock = new Object(); // Lock used for synchronizing id assignment
     private IMyStack<IStatement> executionStack;
-    private IMyDictionary<String, IValue> symbolTable;
+    private SymbolTable symbolTable;
     private IMyList<IValue> output;
 
     private IMyDictionary<String, BufferedReader> fileTable;
@@ -21,7 +27,7 @@ public class ProgramState {
 
     IStatement originalState;
 
-    public ProgramState(IMyStack<IStatement> executionStack, IMyDictionary<String, IValue> symbolTable, IMyList<IValue> out,
+    public ProgramState(IMyStack<IStatement> executionStack, SymbolTable symbolTable, IMyList<IValue> out,
                         IMyDictionary<String, BufferedReader> fileTable, IStatement startingStatement, IMyHeap heap) {
         this.executionStack = executionStack;
         this.symbolTable = symbolTable;
@@ -31,11 +37,18 @@ public class ProgramState {
         originalState = startingStatement.deepCopy();
         // This is the first statement on the stack
         executionStack.push(startingStatement);
+        this.id = getNextId();
+    }
+
+    public static int getNextId(){
+        synchronized (idLock){
+            return idCounter++;
+        }
     }
 
     public void resetProgram() {
         executionStack = new MyStack<>();
-        symbolTable = new MyDictionary<>();
+        symbolTable = new SymbolTable();
         output = new MyList<>();
         fileTable = new MyDictionary<>();
         heap = new MyHeap();
@@ -44,11 +57,19 @@ public class ProgramState {
         executionStack.push(startingStatement);
     }
 
-    public IMyDictionary<String, IValue> getSymbolTable() {
+    public ProgramState oneStep() throws ExecutionException, EvaluationException, ReadWriteException {
+        // Check if the program is completed already or not
+        if (executionStack.isEmpty())
+            throw new ExecutionException("Program execution stack is empty already");
+        // run the current statement that is on top of the stack
+        return executionStack.pop().execute(this);
+    }
+
+    public SymbolTable getSymbolTable() {
         return symbolTable;
     }
 
-    public void setSymbolTable(IMyDictionary<String, IValue> symbolTable) {
+    public void setSymbolTable(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
     }
 
@@ -88,6 +109,10 @@ public class ProgramState {
         this.heap = heap;
     }
 
+    public boolean isCompleted(){
+        return executionStack.isEmpty();
+    }
+
     @Override
     public String toString() {
         String fileTableString = "";
@@ -95,7 +120,7 @@ public class ProgramState {
         for (String reader : fileTable.keys()) {
             fileTableString += reader.toString() + ", ";
         }
-        return "ProgramState:\n" + "Execution Stack: \n" + executionStack.toString() + "SymbolTable: \n" + symbolTable.toString()
+        return "ProgramState " + id + " :\n" + "Execution Stack: \n" + executionStack.toString() + "SymbolTable: \n" + symbolTable.toString()
                 + "Output: \n" + output.toString() + "FileTable: \n" + fileTableString + "Heap: \n" + heap.toString();
     }
 }
