c329842002b691d2720cc19af8d9fff49aefe57c
PotatoOverlord22 <raul.tiron@stud.ubbcluj.ro>
2023-12-18 15:16:44 +0200

    added symbol table

---
+++ src/model/adts/SymbolTable.java
@@ -0,0 +1,57 @@
+package model.adts;
+
+import model.values.IValue;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SymbolTable {
+    private ConcurrentHashMap<String, IValue> map = new ConcurrentHashMap<String, IValue>();
+    public void put(String key, IValue value){
+        map.put(key, value);
+    }
+
+    public IValue get(String key){
+        return map.get(key);
+    }
+
+    public IValue remove(String key){
+        return map.remove(key);
+    }
+
+    public Collection<IValue> values(){
+        return map.values();
+    }
+
+    public Set<String> keys(){
+        return map.keySet();
+    }
+
+    public ConcurrentHashMap<String, IValue> getContent(){
+        return map;
+    }
+
+    public void setContent(ConcurrentHashMap<String, IValue> newContent){
+        map = newContent;
+    }
+
+    public SymbolTable deepCopy(){
+        SymbolTable copy = new SymbolTable();
+        ConcurrentHashMap<String, IValue> content = new ConcurrentHashMap<>();
+        for (Map.Entry<String, IValue> entry : map.entrySet()){
+            content.put(entry.getKey(), entry.getValue().deepCopy());
+        }
+        copy.setContent(content);
+        return copy;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (String key : map.keySet())
+            result.append(key).append(" => ").append(map.get(key).toString()).append('\n');
+        return result.toString();
+    }
+}
---
+++ src/tests/SymbolTableTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import model.adts.SymbolTable;
+import model.values.BoolValue;
+import model.values.IntValue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SymbolTableTest {
+    @Test
+    public void deepCopyTest(){
+        SymbolTable original = new SymbolTable();
+        SymbolTable copy;
+        original.put("v", new IntValue(2));
+        copy = original.deepCopy();
+        original.put("a", new IntValue(3));
+        original.put("v", new BoolValue(true));
+        assertNotEquals(original.get("v"), copy.get("v"));
+        assertNotEquals(original.get("a"), copy.get("a"));
+    }
+}
